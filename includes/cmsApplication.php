ce25bcdc9000cd71dd9e94bc3b7941953df9a1ad
unknown <ulva.erida@gmail.com>
2015-03-18 11:15:04 +0700

    perubahan kedua cms

--- includes/cmsApplication.php
+++ includes/cmsApplication.php
@@ -1,4 +1,14 @@
 <?php
 require_once('cmsBase.php');
 class CmsApplication extends CmsBase{
+	function addcontent(){
+		
+	}
+
+	function viewcontent(){
+		echo'disini akan dibuat fungsi untuk menampilkan konten';
+	}
+	function anyothertask(){
+		echo 'fngsi lainnya'
+	}
 }
